fe22853aa6e7f9a408efb4be6bc90869991372bf
Divu <divyeshkuchhadia@gmail.com>
2024-07-03 21:56:31 +0530

    created header and given style

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import { Header } from "./components/Header";
+
+import '/App.css';
+
+
 function App() {
   return (
     <div>
-      Hello World
+      <Header/>
     </div>
   );
 }
