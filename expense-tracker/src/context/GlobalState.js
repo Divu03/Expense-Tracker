e8e5017836e71b072dcb1243d4a2aa75c5cdad5f
Divu <divyeshkuchhadia@gmail.com>
2024-07-04 18:14:52 +0530

    completed the application

--- expense-tracker/src/context/GlobalState.js
+++ expense-tracker/src/context/GlobalState.js
@@ -2,12 +2,7 @@ import React,{ createContext, useReducer } from "react";
 import AppReducer from './AppReducer'
 
 const intialState ={
-    transactions : [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-    ]
+    transactions : []
 }
 
 //Global context
@@ -26,11 +21,19 @@ export const GlobalProvider = ({children}) => {
             payload : id
         });
     }
+
+    function addTransaction(transaction) {
+        dispatch({
+            type : 'ADD_TRANSACTION',
+            payload : transaction
+        });
+    }
     
     return(
         <GlobalContext.Provider value = {{
             transactions : state.transactions,
-            deleteTransaction
+            deleteTransaction,
+            addTransaction
         }}>{children}
         </GlobalContext.Provider>
     );
