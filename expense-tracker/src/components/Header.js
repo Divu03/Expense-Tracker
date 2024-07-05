fc152f73a3f58f5bf556e587f46023100c39bd7d
Divu <divyeshkuchhadia@gmail.com>
2024-07-05 18:21:15 +0530

    updated header to add account section

--- expense-tracker/src/components/Header.js
+++ expense-tracker/src/components/Header.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 export const Header = () => {
+
+  const logoUrl = "./logo512.png";
   return (
-    <h2>Expense Tracker</h2>
+    <div className='header'>
+      <h2>Expense Tracker</h2>
+      <img src={logoUrl} alt="Logo" className="account-icon" />
+    </div>
   )
 }
