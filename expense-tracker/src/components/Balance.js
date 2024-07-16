017efd6d411eca84808e46bcbd96136ab329b0ea
Divyesh Kuchhadia <96073185+Divu03@users.noreply.github.com>
2024-07-16 14:56:39 +0530

    Update Balance.js to fix the number after decimal

--- expense-tracker/src/components/Balance.js
+++ expense-tracker/src/components/Balance.js
@@ -9,7 +9,7 @@ export const Balance = () => {
     return (
         <div className='balanceDiv'>
             <h4>Your Balance</h4>
-            <h1>${total}</h1>
+            <h1>${total.toFixed(2)}</h1>
         </div>
     );
 };
